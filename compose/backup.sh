860c4b3b94096a547e046ab118c2b39da8fe7be3
caberger <c.aberger@htl-leonding.ac.at>
2024-06-15 07:41:15 +0200

    remove default resource to allow import of realm json files

--- compose/backup.sh
+++ compose/backup.sh
@@ -3,6 +3,10 @@
 # to use this script backup container must be running, so start the following before:
 # docker compose -f backup-keycloak.yml up
 
+set -e
 mkdir -p ./target
 docker compose exec postgres pg_dump --dbname=keycloak --username=keycloak | gzip > ./target/keycloak.sql.gz
 docker compose cp backup:/export ./target
+
+cp ./target/export/*.json ./keycloak/import
+
--- compose/dev.sh
+++ compose/dev.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+docker compose -f postgres.yaml pull
 pushd keycloak
 docker build --tag keycloak .
 popd
