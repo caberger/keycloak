2a4de9171206e0fe589e6a8f3dbd6982c42024fe
caberger <c.aberger@htl-leonding.ac.at>
2024-06-16 19:18:22 +0200

    show how to extract claims in rust

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,7 +3,7 @@ use port_scanner::scan_port;
 use dotenvy::dotenv;
 use actix_web::{middleware, web, App, HttpServer};
 use pubkey::load_key;
-use actix_web_middleware_keycloak_auth::{AlwaysReturnPolicy, DecodingKey, KeycloakAuth, Role};
+use actix_web_middleware_keycloak_auth::{DecodingKey, KeycloakAuth};
 
 const PORT: i32 = 8080;
 
@@ -13,6 +13,7 @@ mod posts;
 mod models;
 mod schema;
 mod pubkey;
+mod claims;
 
 async fn serve() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
@@ -22,25 +23,16 @@ async fn serve() -> io::Result<()> {
 
     HttpServer::new(move || {
         let auth = KeycloakAuth::default_with_pk(DecodingKey::from_rsa_pem(pub_key.as_bytes()).unwrap());
-        let keycloak_editor_auth = KeycloakAuth {
-            detailed_responses: true,
-            passthrough_policy: AlwaysReturnPolicy,
-            keycloak_oid_public_key: DecodingKey::from_rsa_pem(pub_key.as_bytes()).unwrap(),
-            required_roles: vec![
-                Role::Realm { role: "editor".to_owned() }, // The "admin" realm role must be provided in the JWT
-            ],
-        };
         App::new()
             .wrap(middleware::Logger::default()) // enable logger - always register actix-web Logger middleware last
             .service(web::scope("/public")
                 .service(hello::get)
             )
             .service(web::scope("/api")
-                .service(posts::get_published)   
-                .wrap(auth)
                 .service(posts::get)
-                .wrap(keycloak_editor_auth)
+                .wrap(auth)
             )
+            
     })
     .bind(bind_addr)?
     .run()
--- backend/src/posts.rs
+++ backend/src/posts.rs
@@ -1,16 +1,16 @@
 use actix_web::{get, HttpResponse};
+use actix_web_middleware_keycloak_auth::KeycloakClaims;
 use backend::establish_connection;
-use crate::{constants::APPLICATION_JSON, models::Post, schema, };
+use crate::{claims::{ClaimsFromJwt, EDITOR_ROLE}, constants::APPLICATION_JSON, models::Post, schema };
 use schema::posts::dsl::*;
 use diesel::prelude::*;
 
-#[get("/posts/published")]
-pub async fn get_published() -> HttpResponse {
-    load(true)
-}
 #[get("/posts")]
-pub async fn get() -> HttpResponse {
-    load(false)
+pub async fn get(claims: KeycloakClaims<ClaimsFromJwt>) -> HttpResponse {
+    let roles = &claims.realm_access.roles;
+    let published_only = !roles.contains(&String::from(EDITOR_ROLE));
+    println!("{} allowed to see unpublished posts: {}", claims.preferred_username, !published_only);
+    load(published_only)
 }
 fn load(published_only: bool) -> HttpResponse {
     let connection = &mut establish_connection();
@@ -22,19 +22,16 @@ fn load(published_only: bool) -> HttpResponse {
     let filtered_results =
         stmt
         .filter(published.eq(published_only));
-    let results = if published_only {filtered_results.load(connection)} else {all_results.load(connection)};
-    let decoded = results.expect("Error loading posts");
-    //let results = expected_results.load(connection).expect("Error loading posts");
-    /*
-    let results = posts
-        .limit(50)
-        .filter(published.eq(published_only))
-        .select(Post::as_select())
-        .load(connection)
-        .expect("Error loading posts");
-    */
+    let results =
+        if published_only {
+            filtered_results.load(connection)
+        } else {
+            all_results.load(connection)
+        }
+        .expect("Error loading posts")
+    ;
     HttpResponse::Ok()
         .content_type(APPLICATION_JSON)
-        .json(decoded)
+        .json(results)
 }
 
