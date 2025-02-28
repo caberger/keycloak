d8a79c51d6679aa72d9f2e4c0bdd63e919ce0222
caberger <c.aberger@htl-leonding.ac.at>
2025-02-28 13:35:10 +0100

    backup

--- compose/backup-keycloak.sh
+++ compose/backup-keycloak.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+mkdir -p ./target/export/
 
 # to use this script backup container must be running, so start the following before:
-docker compose --detach --file backup-keycloak.yaml up
-
-echo "Waiting  to launch on 8080..."
-
-while ! nc -z localhost 8080; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-mkdir -p ./target
-docker compose exec postgres pg_dump --dbname=keycloak --username=keycloak | gzip > ./target/keycloak.sql.gz
-docker compose cp backup:/export ./target
-
-cp ./target/export/*.json ./keycloak/import
-
+docker compose --file backup-keycloak.yaml up --build --detach
+#docker compose exec postgres pg_dump --dbname=keycloak --username=keycloak | gzip > ./target/keycloak.sql.gz
 docker compose --file backup-keycloak.yaml down
+echo "TODO: cp ./target/export/*.json ./keycloak/import"
