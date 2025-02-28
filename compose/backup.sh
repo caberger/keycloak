d3b6ad737eb07cdb338f51058b4de586d398c52f
caberger <c.aberger@htl-leonding.ac.at>
2025-02-28 12:45:54 +0100

    backup script

--- compose/backup.sh
+++ compose/backup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
-rm -rf ./target && mkdir -p ./target
+
+mkdir -p ./target
+
+echo "Waiting for postgres..."
+
+while ! nc -z localhost 5432; do
+    echo "wait for postgres..."
+    sleep 1
+done
+
 docker compose exec postgres pg_dump --dbname=keycloak --username=keycloak | gzip > ./target/keycloak.sql.gz
 docker compose exec postgres pg_dump --dbname=demo --username=demo | gzip > ./target/demo.sql.gz
 
