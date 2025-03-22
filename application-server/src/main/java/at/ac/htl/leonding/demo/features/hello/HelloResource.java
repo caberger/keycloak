438489a212c9f4b01df0a52acecc9cf91b229f93
caberger <c.aberger@htl-leonding.ac.at>
2025-03-22 07:53:04 +0100

    inject request

--- application-server/src/main/java/at/ac/htl/leonding/demo/features/hello/HelloResource.java
+++ application-server/src/main/java/at/ac/htl/leonding/demo/features/hello/HelloResource.java
@@ -2,15 +2,24 @@ package at.ac.htl.leonding.demo.features.hello;
 
 import java.time.LocalDateTime;
 
+import org.jboss.resteasy.spi.HttpRequest;
+
+import io.quarkus.logging.Log;
 import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
 
 @Path("/hello")
 @PermitAll
 public class HelloResource {
+    @Context
+    HttpRequest request;
+
     @GET
     public Hello hello() {
+        var message = String.format("send hello to client %s", request.getRemoteHost());
+        Log.info(message);
         return new Hello("hello, world", LocalDateTime.now());
-    } 
+    }
 }
--- compose/start.sh
+++ compose/start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker compose up --build
+docker compose up --build --detach
+watch --no-title --no-wrap docker compose ps
