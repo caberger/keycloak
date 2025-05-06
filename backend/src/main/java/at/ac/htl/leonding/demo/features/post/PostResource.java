61c93c3e54b17ae460cfee489e6f907aa8fb3482
caberger <c.aberger@htl-leonding.ac.at>
2025-05-06 20:47:41 +0200

    show return type in api doc

--- backend/src/main/java/at/ac/htl/leonding/demo/features/post/PostResource.java
+++ backend/src/main/java/at/ac/htl/leonding/demo/features/post/PostResource.java
@@ -1,23 +1,24 @@
 package at.ac.htl.leonding.demo.features.post;
 
+import java.util.List;
+
+import at.ac.htl.leonding.demo.features.post.PostMapper.PostDto;
 import at.ac.htl.leonding.demo.lib.CsvMessageBodyProvider;
-import at.ac.htl.leonding.demo.lib.Responses;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
 @Path("/posts")
 @Produces({MediaType.APPLICATION_JSON, CsvMessageBodyProvider.MEDIA_TYPE})
 public class PostResource {
     @GET
     @Path("/published")
-    public Response published() {
-        return Responses.ok(PostMapper.toResource(PostRepository.allPublishedPosts()));
+    public List<PostDto> published() {
+        return PostMapper.toResource(PostRepository.allPublishedPosts());
     }
     @GET
-    public Response all() {
-        return Responses.ok(PostMapper.toResource(PostRepository.all()));
+    public List<PostDto> all() {
+        return PostMapper.toResource(PostRepository.all());
     }   
 }
