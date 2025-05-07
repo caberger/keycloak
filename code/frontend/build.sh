944be6a377b6180df904288e197e72e33ca2327c
caberger <c.aberger@htl-leonding.ac.at>
2025-05-07 08:49:55 +0200

    refactor folders

---
+++ code/frontend/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+npm install --no-fund --no-audit
+npm audit --omit dev
+
+npm run build
+DEST=../application-server/src/main/resources/META-INF/resources/
+mkdir -p $DEST
+cp -r ./dist/ $DEST
+cp -r ./html $DEST
+echo "frontend deployed to $DEST"
--- start.sh
+++ start.sh
@@ -7,27 +7,28 @@ set -e
 
 docker ps
 
-pushd compose
+pushd ./code/compose
     docker compose down
 popd
 
-pushd ./frontend
+pushd ./code/frontend
     ./build.sh
 popd
 
-pushd ./backend
+
+pushd ./code/application-server
     echo "install backend dependencies..."
     mvn dependency:resolve
 popd
 
-pushd ./compose
+pushd ./code/compose
     echo "start docker compose..."
     bash -c ./start.sh
 popd
 
 echo "==="
 echo "= docker compose started."
-echo "= 1.) open a new terminal in $PWD/backend and run: → ${bold}./start.sh${normal}"
+echo "= 1.) open a new terminal in $PWD/code/application-server and run: → ${bold}./start.sh${normal}"
 echo "= you can start the application at http://localhost:8080"
-echo "= 2.) for frontend development on http://localhost:4200 you can open a new terminal in $PWD/frontend and run: → ${bold}npm start${normal}"
+echo "= 2.) for frontend development on http://localhost:4200 you can open a new terminal in $PWD/code/frontend and run: → ${bold}npm start${normal}"
 echo "==="
